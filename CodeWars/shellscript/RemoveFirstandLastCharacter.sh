b1696acb93ab0d1b321a95f85d3f997ce898a5fa
lijingqi <ljq@qq.com>
2020-06-25 11:36:57 +0800

    rename shell folder

--- CodeWars/shellscript/RemoveFirstandLastCharacter.sh
+++
@@ -1,8 +0,0 @@
-#https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0
-function removeChar() {
-      str=$1
-      len1=${#str}
-      len1=$[len1-1]
-      echo $str | cut -c  2-$len1
-}
-removeChar $1
--- CodeWars/shellscript/WhattimeisitinJapan.sh
+++
@@ -1,2 +0,0 @@
-#https://www.codewars.com/kata/59c53183aeb2842585000116
-TZ='Asia/Tokyo' date "+%Y-%m-%dT%H:%M%z"
