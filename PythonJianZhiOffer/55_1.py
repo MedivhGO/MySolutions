1a5ca32904c9a057df329eeb1a00585600d8928b
Lee <lijingqi93@foxmail.com>
2022-01-30 20:15:58 +0800

    add q55_1 solution

---
+++ PythonJianZhiOffer/55_1.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        ans = 0
+        queue = collections.deque()
+        if root is not None:
+            queue.append(root)
+        while len(queue) != 0:
+            size = len(queue)
+            ans += 1
+            while size != 0:
+                next_level = queue.popleft()
+                if next_level.left is not None:
+                    queue.append(next_level.left)
+                if next_level.right is not None:
+                    queue.append(next_level.right)
+                size -= 1
+        return ans
+
+# BFS
+
+# 时间复杂度 O(n)
+# 空间复杂度 O(n) 最差情况下, (当树平衡时), 队列queue同时存储N/2个节点.
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        return 0 if root is None else 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+# 后序遍历
+
+# 时间复杂度 O(n)
+# 空间复杂度 O(n) 最差情况下递归调用深度是一个单边树.
