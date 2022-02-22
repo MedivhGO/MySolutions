88abf491705e59399fa797a18a9e5103813d1ca9
MedivhGO <lijingqi93@gmail.com>
2022-02-22 17:44:50 +0800

    add q0078 solution

---
+++ ReLeetCode/0078.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        for i in range(len(nums) + 1):
+            for tmp in itertools.combinations(nums, i):
+                res.append(tmp)
+        return res
+
+# 使用Python提供的库函数
+# itertools.combinations(iterable,r) 这个函数用来生成指定数目r的元素不重复的所有组合。
+
+# 时间复杂度 O(n*2^n) 因为一共有2^n个子集，生成每个子集的复杂度为n，所以一共O(n*2^n)
+# 空间复杂度 O(n)
