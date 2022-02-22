a3213d91dccfa991a4a000b9117548902b904400
MedivhGO <lijingqi93@gmail.com>
2022-02-22 17:59:00 +0800

    add more solution for q0078

--- ReLeetCode/0078.py
+++ ReLeetCode/0078.py
@@ -11,3 +11,15 @@ class Solution:
 
 # 时间复杂度 O(n*2^n) 因为一共有2^n个子集，生成每个子集的复杂度为n，所以一共O(n*2^n)
 # 空间复杂度 O(n)
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for i in nums:
+            res = res + [[i] + num for num in res]
+        return res
+
+# 迭代法
+
+# 时间复杂度 O(n*2^n) 因为一共有2^n个子集，生成每个子集的复杂度为n，所以一共O(n*2^n)
+# 空间复杂度 O(n)
