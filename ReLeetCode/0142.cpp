449a2e98f0f189d8274fa638ab18e2fb0d89e587
MedivhGO <lijingqi93@gmail.com>
2023-02-17 15:03:31 +0800

    add explanation about 0142 and jianzhioffer 06 questions

--- ReLeetCode/0142.cpp
+++ ReLeetCode/0142.cpp
@@ -24,8 +24,25 @@ public:
     }
 };
 
-// 首先快慢指针找到相遇点
-// 
+// 首先快慢指针找到相遇点，如下图所示为 p
+/*
+ *                     xxxxxxxxxxxxxxxx
+ *                z xxx       p       xx
+ *                 xx                  x
+ *                 x                   x
+ *         x       x                    x
+ *  xxxxxxxxxxxxxxxx                    x y
+ * m              nx                    x
+ *                 xx                  xx
+ *                   xx                x
+ *                    xx             xxx
+ *                     xxxxxxxxxxxxxx
+ */
+// 如图可知，环的入口节点为 n。
+// 记 m -> n 的长度为 x，n -> p 的长度为 y， p -> n 的长度为 z
+// 由快慢指针的性质有如下等式
+// 2 (x + y) = x + y + z + y
+// 化简后有 x = z
 // 所以相遇点到环入口节点的距离与从头节点开始到入口节点的距离是相同的，他们在相遇时，就是入口节点的位置
 
 // 时间复杂度 O(n)
