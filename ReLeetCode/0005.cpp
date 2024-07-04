63e32d5103b88eb4661e933162032cf99b93259d
MedivhGO <lijingqi93@gmail.com>
2024-07-04 16:53:25 +0800

    add solutions

--- ReLeetCode/0005.cpp
+++ ReLeetCode/0005.cpp
@@ -32,3 +32,35 @@ public:
         return s.substr(begin, maxlen);
     }
 };
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        if (s.empty()) {
+            return s;
+        }
+        if (s.size() < 2) {
+            return s;
+        }
+        int start = 0, end = 0;
+        int len = INT_MIN;
+        for (int i = 0; i < s.size(); ++i) {
+            int len1 = longestHelper(i, i, s);
+            int len2 = longestHelper(i, i+1, s);
+            if (len < max(len1, len2)) {
+                len = max(len1, len2);
+                start = i - (len - 1) / 2;
+                end = i + len/2;
+            }
+        }
+        return s.substr(start, end - start + 1);
+    }
+    
+    int longestHelper(int m, int n, string s) {
+        while (m >= 0 && n < s.size() && s[m] == s[n]) {
+            m--;
+            n++;
+        }
+        return n - m - 1;
+    }
+};
